e9f0340ab161f339c01195010182c5a72514b766
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-04-19 14:13:15 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -12,10 +12,10 @@ terraform {
   }
 }
 
-# provider "azurerm" {
-#   features {
-#     resource_group {
-#       prevent_deletion_if_contains_resources = false
-#     }
-#   }
-# }
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
